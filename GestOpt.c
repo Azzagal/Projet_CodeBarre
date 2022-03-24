6b3c7a8e5b868d6eed859828bdd841e5b9c8eed0
Azzagal <pierre.lorenzen@gmail.com>
2022-03-24 18:22:19 +0100

    fonction et librairie qui gère les arguments a l'exécution

---
+++ GestOpt.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <getopt.h>
+#include "GestOpt.h"
+#include <string.h>
+#include <assert.h>
+
+void gestion_option(char *optstring, char *format, char *entree, char *sortie , int argc, char *argv[]){
+  assert(optstring != NULL && format != NULL && entree != NULL && sortie != NULL);
+
+  printf("\n");
+
+  int val;
+
+  while((val=getopt(argc, argv, optstring))!=EOF){
+    switch(val){
+      case 'f':
+        printf("format: %s\n",optarg);  
+        memcpy(format, optarg, strlen(optarg)+1);
+        break;
+      case 'i':
+        printf("entree: %s\n", optarg);
+        memcpy(entree, optarg, strlen(optarg)+1);
+        break;
+      case 'o':
+        printf("sortie: %s\n", optarg); 
+        memcpy(sortie, optarg, strlen(optarg)+1);
+        break;
+      case '?':
+        printf("unknown option: %c\n", optopt); break;
+      case ':':
+        printf("missing arg: %c\n", optopt); break;
+    }//fin switch
+  }//fin while
+
+  printf("\n");
+}//Fin gestion_option()
---
+++ GestOpt.h
@@ -0,0 +1,26 @@
+/**
+ * \file Gestopt.h
+ *
+ * \brief Ce fichier contient la déclaration d'une fonction qui gère les options passées en arguments
+ *
+ * \author Lorenzen Pierre s203724
+ * \date 24/03/2022
+ * 
+ */
+#ifndef __GESTOPT__
+#define __GESTOPT__
+/**
+ * \fn void gestion_option(char *optstring, char *format, char *entree, char *sortie, int argc, char *argv[])
+ *
+ * \brief Gère les options introduites à l'execution d'un programme.
+ *
+ * \param optstring est la chaine de caractères qui défini les paramètres à prendre en compte.
+ * \param format est le format du fichier passé en argument.
+ * \param entree est l'emplacement du fichier passé en argument.
+ * \param sortie est le nom du fichier de sortie.
+ * \param argc le premier argument de la fonction main.
+ * \param argv le deuxième argument de la fonxtion main.
+ *
+ */
+void gestion_option(char *optstring, char *format, char *entree, char *sortie, int argc, char *argv[]);
+#endif
